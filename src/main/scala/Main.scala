82fe0afd761bef5948cac40e79b245de757c3375
Hayato-Kokubu <kokubu.hayato@gmail.com>
2019-08-25 11:33:55 +0900

    getting started! Hello World!

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,27 +1,54 @@
-import scala.concurrent.{Await, Future}
-import scala.concurrent.ExecutionContext.Implicits.global
+// We need a scheduler whenever asynchronous
+// execution happens, substituting your ExecutionContext
+
+
+import monix.execution.Scheduler.Implicits.global
+
+
+import scala.concurrent.Await
 import scala.concurrent.duration._
+
+import monix.reactive.Observable
+
+import monix.eval.Task
+
 
 object Main extends App {
 
-//  val fur1 = Future.apply{println("fur1 culc!!"); 1}
-//  val fur2 = Future.apply{println("fur2 culc!!"); 2}
-//
-//
-//  val fur3 =
-//    for{
-//      n <- fur1
-//      m <- fur2
-//    }yield n + m
+  // A specification for evaluating a sum,
+  // nothing gets triggered at this point! <- to execute this I must do something
+  val task = Task{1 + 1}
 
 
-  val fur3 = for{
-    n <- Future.apply{println("fur1 culc!!"); 1}
-    m <- Future.apply{println("fur2 culc!!"); 2}
-  }yield n + m
+  // runToFuture が見つからないためコメントアウト
+  //  val future = task.runToFuture
+  // これで、Future が得られるとのこと
 
 
-  Await.result(fur3, 1.second)
+  // Nothing happens here, as observable is lazily
+  // evaluated only when the subscription happens!
+  val tick = {
+    Observable.interval(1.second) // Observable[Long]
+      .filter(_ % 2 == 0)
+      // Obserable has map and flatMap
+      .map(_ * 2)
+      .flatMap(x => Observable.fromIterable(Seq(x,x)))
+      // only take the first 5 elements, then stop
+      .take(5)
+      // to print the generated events to console
+      .dump("Out")
+  }
+
+  // ↑ now tick is not executed.
+
+  val cancelable = tick.subscribe() // Cancelable
+
+
+//  Thread.sleep(10000)
+
+  cancelable.cancel()
+
+
 
 
 
