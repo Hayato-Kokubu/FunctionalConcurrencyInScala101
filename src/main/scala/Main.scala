e74778a9d75083fc5d4cc3d5a639d346d627e0fb
Hayato-Kokubu <kokubu.hayato@gmail.com>
2019-08-25 12:26:41 +0900

    tutorial Parallel Processing  The Naive Way

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,11 +1,22 @@
-import monix.eval.Task
-import cats.Monad
+//import monix.eval.Task
+import monix.execution.Scheduler.Implicits.global
+
+
+import monix.eval._
+import monix.reactive._
 
 object Main extends App {
 
-  implicitly[Monad[Task]] // 正体はTask.catsAsync
+  val items = 0 until 1000
+
+  // The list of all tasks needed for execution
+  val tasks = items.map(i => Task(i * 2))
 
 
-
+  val aggregate = Task.gather(tasks).map(_.toList)
+  val aggregateNoOrder = Task.gatherUnordered(tasks).map(_.toList)
+  // Evaluation:
+  aggregate.foreach(println)
+  aggregateNoOrder.foreach(println)
 
 }
